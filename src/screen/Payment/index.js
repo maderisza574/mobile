3e8153669facd848fed5960a165e1eb0edd5471f
maderisza <maderisza@gmail.com>
2022-11-16 02:48:17 +0700

    add: page payment

--- src/screen/Payment/index.js
+++ src/screen/Payment/index.js
@@ -1,12 +1,81 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, Image} from 'react-native';
 import styles from './styles';
+import {RadioButton} from 'react-native-paper';
+import Card from '../../assets/img/card.png';
+import Bank from '../../assets/img/bank.png';
+import Market from '../../assets/img/market.png';
 
 export default function Payment(props) {
+  const [checked, setChecked] = React.useState(false);
+  const [checked1, setChecked1] = React.useState(false);
+  const [checked2, setChecked2] = React.useState(false);
+  const [checked3, setChecked3] = React.useState(false);
+
   return (
     <View>
       <View style={styles.container}>
-        <Text>Payment Method</Text>
+        <Text style={styles.tittlescreen}>Payment Method</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: 50,
+          }}>
+          <RadioButton
+            status={checked ? 'checked' : 'unchecked'}
+            onPress={() => {
+              setChecked(!checked);
+            }}
+          />
+          <Image source={Card} />
+          <Text style={styles.label}>Card</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: 50,
+          }}>
+          <RadioButton
+            status={checked ? 'checked' : 'unchecked'}
+            onPress={() => {
+              setChecked1(!checked1);
+            }}
+          />
+          <Image source={Bank} />
+          <Text style={styles.label}>Bank Transfer</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: 50,
+          }}>
+          <RadioButton
+            status={checked ? 'checked' : 'unchecked'}
+            onPress={() => {
+              setChecked2(!checked2);
+            }}
+          />
+          <Image source={Market} />
+          <Text style={styles.label}>Retail</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: 50,
+          }}>
+          <RadioButton
+            status={checked ? 'checked' : 'unchecked'}
+            onPress={() => {
+              setChecked3(!checked3);
+            }}
+          />
+          <Image source={Card} />
+          <Text style={styles.label}>E-Money</Text>
+        </View>
       </View>
       <Button
         title="Home Screen"
--- src/screen/Payment/styles.js
+++ src/screen/Payment/styles.js
@@ -2,6 +2,12 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    backgroundColor: '#3366FF',
+    backgroundColor: 'white',
+  },
+  tittlescreen: {
+    fontFamily: 'Poppins-Reguler',
+    fontSize: 20,
+    color: 'black',
+    marginLeft: 30,
   },
 });
